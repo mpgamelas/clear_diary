2bc9c05305f55ad6b947832e180a4a1ef77e69e9
Mario Gamelas <mariogamelas.info@gmail.com>
2020-10-12 14:32:24 -0300

    more work on diary entry screen

--- lib/screens/diary_entry.dart
+++ lib/screens/diary_entry.dart
@@ -1,24 +1,157 @@
+import 'package:clear_diary/values/strings.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_tagging/flutter_tagging.dart';
 
-class DiaryEntry extends StatefulWidget {
+class DiaryEntry extends StatelessWidget {
   static const String id = 'diary_entry_screen';
 
   @override
-  _DiaryEntryState createState() => _DiaryEntryState();
-}
-
-class _DiaryEntryState extends State<DiaryEntry> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('titulo teste'),
-      ),
-      body: Container(
-        child: Center(
-          child: Text('Teste texo'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(Strings.newEntry),
         ),
+        body: DiaryEntryBody(),
       ),
     );
   }
+}
+
+class DiaryEntryBody extends StatefulWidget {
+  @override
+  _DiaryEntryBodyState createState() => _DiaryEntryBodyState();
+}
+
+class _DiaryEntryBodyState extends State<DiaryEntryBody> {
+  final GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
+
+  var options = ["Option 1", "Option 2", "Option 3"];
+
+  final String entryDateKey = 'entry_date';
+  final String entryTitleKey = 'entry_title';
+  final String entryTagsKey = 'entry_tags';
+  final String entryBodyKey = 'entry_body';
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        FormBuilder(
+          key: _fbKey,
+          initialValue: {
+            entryDateKey: DateTime.now(),
+          },
+          autovalidateMode: AutovalidateMode.disabled,
+          child: Column(
+            children: <Widget>[
+              FormBuilderDateTimePicker(
+                attribute: entryDateKey,
+                inputType: InputType.date,
+                format: DateFormat("dd-MM-yyyy"),
+                decoration: InputDecoration(labelText: Strings.entryDate),
+              ),
+              FormBuilderTextField(
+                attribute: entryTitleKey,
+                decoration: InputDecoration(labelText: Strings.entryTitle),
+                validators: [
+                  FormBuilderValidators.maxLength(240),
+                ],
+              ),
+              FormBuilderCustomField(
+                attribute: entryTagsKey,
+                validators: [],
+                formField: FormField(
+                  enabled: true,
+                  builder: (FormFieldState<dynamic> field) {
+                    return InputDecorator(
+                      decoration: InputDecoration(
+                        labelText: "Select option",
+                        contentPadding: EdgeInsets.only(top: 10.0, bottom: 0.0),
+                        border: InputBorder.none,
+                        errorText: field.errorText,
+                      ),
+                      child: Container(
+                        height: 200,
+                        child: CupertinoPicker(
+                          itemExtent: 30,
+                          children: options.map((c) => Text(c)).toList(),
+                          onSelectedItemChanged: (index) {
+                            field.didChange(options[index]);
+                          },
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+        Row(
+          children: <Widget>[
+            MaterialButton(
+              child: Text("Submit"),
+              onPressed: () {
+                if (_fbKey.currentState.saveAndValidate()) {
+                  print(_fbKey.currentState.value);
+                }
+              },
+            ),
+            MaterialButton(
+              child: Text("Reset"),
+              onPressed: () {
+                _fbKey.currentState.reset();
+              },
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+/// LanguageService
+class LanguageService {
+  /// Mocks fetching language from network API with delay of 500ms.
+  static Future<List<Language>> getLanguages(String query) async {
+    await Future.delayed(Duration(milliseconds: 500), null);
+    return <Language>[
+      Language(name: 'JavaScript', position: 1),
+      Language(name: 'Python', position: 2),
+      Language(name: 'Java', position: 3),
+      Language(name: 'PHP', position: 4),
+      Language(name: 'C#', position: 5),
+      Language(name: 'C++', position: 6),
+    ]
+        .where((lang) => lang.name.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+  }
+}
+
+/// Language Class
+class Language extends Taggable {
+  ///
+  final String name;
+
+  ///
+  final int position;
+
+  /// Creates Language
+  Language({
+    this.name,
+    this.position,
+  });
+
+  @override
+  List<Object> get props => [name];
+
+  /// Converts the class to json string.
+  String toJson() => '''  {
+    "name": $name,\n
+    "position": $position\n
+  }''';
 }
--- lib/values/strings.dart
+++ lib/values/strings.dart
@@ -3,4 +3,8 @@ class Strings {
   static const String homeScreenTitle = 'Home Screen';
   static const String addEntry = 'Add entry';
   static const String settings = 'Settings';
+  static const String newEntry = 'New Entry';
+  static const String entryTitle = 'Entry Title';
+  static const String entryText = 'Entry Text';
+  static const String entryDate = 'Date of Entry';
 }
